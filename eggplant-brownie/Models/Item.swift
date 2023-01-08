78a10feabce52d0be7e4e1108ec0389af22aa40c
Arilson F J Silva <77757315+ArilsonFJS@users.noreply.github.com>
2023-01-08 18:16:41 -0300

    implementacao NSCoding para salvar os dados inseridos no device em um arquivo

--- eggplant-brownie/Models/Item.swift
+++ eggplant-brownie/Models/Item.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class Item: NSObject {
+class Item: NSObject, NSCoding {
     
     var nome: String
     var calorias: Double
@@ -15,5 +15,20 @@ class Item: NSObject {
     init(nome: String, calorias: Double) {
         self.nome = nome
         self.calorias = calorias
+    }
+    
+    //MARK: - NSConding
+    
+    //Metodo que transforma os atributos da classe em bytes
+    func encode(with coder: NSCoder) {
+        //forKey serve como uma chave para resgatar a informação.
+        coder.encode(nome, forKey: "nome")
+        coder.encode(calorias, forKey: "calorias")
+    }
+    
+    //Metodo que decodifica os atributos que foram transformado em bytes
+    required init?(coder: NSCoder) {
+        nome = coder.decodeObject(forKey: "nome") as! String
+        calorias = coder.decodeDouble(forKey: "calorias")
     }
 }
--- eggplant-brownie/Models/Refeicao.swift
+++ eggplant-brownie/Models/Refeicao.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class Refeicao: NSObject {
+class Refeicao: NSObject, NSCoding {
 
     let nome: String
     let felicidade: Int
@@ -17,6 +17,23 @@ class Refeicao: NSObject {
         self.nome = nome
         self.felicidade = felicidade
         self.itens = itens
+    }
+    
+    //MARK: - NSConding
+    
+    //Metodo que transforma os atributos da classe em bytes
+    func encode(with coder: NSCoder) {
+        //forKey serve como uma chave para resgatar a informação.
+        coder.encode(nome, forKey: "nome")
+        coder.encode(felicidade, forKey: "felicidade")
+        coder.encode(itens,forKey:"itens")
+    }
+    
+    //Metodo que decodifica os atributos que foram transformado em bytes
+    required init?(coder: NSCoder) {
+        nome = coder.decodeObject(forKey: "nome") as! String
+        felicidade = coder.decodeInteger(forKey: "felicidade")
+        itens = coder.decodeObject(forKey: "itens") as! Array<Item>
     }
     
     func totalCalorias()-> Double{
